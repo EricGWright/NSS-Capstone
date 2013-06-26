efd1c13d5c5f7b4cabdfc97a3f124f78a5eb7e19
EricGWright <musicmarketer@gmail.com>
2013-06-26 10:52:23 -0500

    ENV SECRET_TOKEN added

--- config/initializers/secret_token.rb
+++ config/initializers/secret_token.rb
@@ -4,4 +4,8 @@
 # If you change this key, all old signed cookies will become invalid!
 # Make sure the secret is at least 30 characters and all random,
 # no regular words or you'll be exposed to dictionary attacks.
-NSSCapstone::Application.config.secret_token = 'd9ccb022377e379a342582626ae022b86e915cc10c1d9de53d5204c1c6afa6c7f783329a94a7cf8ed7db60b765fdc712d3cd1daa78b18d258243c80c877f0386'
+if Rails.env.development? or Rails.env.test?
+  NSSCapstone::Application.config.secret_token = 'd9ccb022377e379a342582626ae022b86e915cc10c1d9de53d5204c1c6afa6c7f783329a94a7cf8ed7db60b765fdc712d3cd1daa78b18d258243c80c877f0386'
+else
+  NSSCapstone::Application.config.secret_token = ENV['SECRET_TOKEN']
+end
